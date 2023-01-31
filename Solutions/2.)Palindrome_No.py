d559684b7837609db24dfea45be0b334c89c0f1e
Dhruvxarch <Anuragvishwas19@gmail.com>
2023-01-31 17:36:04 +0530

    Palindrome number

---
+++ Solutions/2.)Palindrome_No.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        s=str(x)
+        a=s[::-1]
+        reverse= False
+        if s==a:
+            reverse=True
+        else:
+            reverse
+            
+        return (reverse)
