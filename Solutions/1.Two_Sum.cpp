34441df6f3b47df11689d49fb3ebc47e91c4bfa2
ANURAG VISHWAS <ANURAGVISHWAS19@GMAIL.COM>
2023-02-01 20:04:03 +0530

    Update 1.Two_Sum.cpp

--- Solutions/1.Two_Sum.cpp
+++ Solutions/1.Two_Sum.cpp
@@ -16,3 +16,18 @@ public:
     return result;
     }
 };
+
+
+
+
+// Explanation:
+
+// 1. Initialize an unordered map map and a result vector result.
+// 2. Loop through the nums vector.
+// 3. For each iteration, calculate the complement value (target minus the current element nums[i]).
+// 4. Check if the complement is in the map. If it is, push the indices of both elements (the complement and the current element) into the result vector and break the loop.
+// 5. If the complement is not in the map, insert the current element into the map with its index as the value.
+// 6. After the loop, return the result vector.
+    
+// This implementation only requires a single pass through the nums vector and has a time complexity of O(n) and a space complexity of O(n).
+
