842196fe5efbfa5e45c5393a1265c29f0f04723f
Dhruvxarch <Anuragvishwas19@gmail.com>
2023-03-24 04:00:13 +0530

    1431.kids with greatest number of candies

--- Solutions/1431.kids_with_Greatest_no._of_candies.cpp
+++ Solutions/1431.kids_with_Greatest_no._of_candies.cpp
@@ -1,30 +1,28 @@
-class Solution
-{
-public:
-    vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
-    {
-        int max = candies[0];
-        int n = candies.size();
-        vector<bool> ans(n);
-        for (int i = 0; i < n; i++)
-        {
-            if (candies[i] > max)
-            {
-                max = candies[i];
-            }
-        }
 
-        for (int i = 0; i < n; i++)
+public:
+vector<bool> kidsWithCandies(vector<int> &candies, int extraCandies)
+{
+    int max = candies[0];
+    int n = candies.size();
+    vector<bool> ans(n);
+    for (int i = 0; i < n; i++)
+    {
+        if (candies[i] > max)
         {
-            if (candies[i] + extraCandies >= max)
-            {
-                ans[i] = true;
-            }
-            else
-            {
-                ans[i] = false;
-            }
+            max = candies[i];
         }
-        return ans;
     }
-};
+
+    for (int i = 0; i < n; i++)
+    {
+        if (candies[i] + extraCandies >= max)
+        {
+            ans[i] = true;
+        }
+        else
+        {
+            ans[i] = false;
+        }
+    }
+    return ans;
+}
