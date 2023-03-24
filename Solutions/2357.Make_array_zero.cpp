626b84d62917a4db1994cb1a1f9a02e912572a0f
Dhruvxarch <Anuragvishwas19@gmail.com>
2023-03-24 17:09:26 +0530

    changed the code a little bit

--- Solutions/2357.Make_array_zero.cpp
+++ Solutions/2357.Make_array_zero.cpp
@@ -5,8 +5,8 @@ public:
         int ans=0;
         vector<int> v(101);
 
-        for(int i =0;i<n;i++){
-            v[nums[i]]=1;
+        for(auto a:nums){
+            v[a]=1;
         }
 
         for(int i =1;i<=100;i++){
