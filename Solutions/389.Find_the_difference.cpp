756d4b7e467991d52d90f0d300dea4b7dc8a2028
ANURAG VISHWAS <ANURAGVISHWAS19@GMAIL.COM>
2024-07-27 22:49:17 +0530

    Updated 389.Find_the_difference.cpp

--- Solutions/389.Find_the_difference.cpp
+++ Solutions/389.Find_the_difference.cpp
@@ -2,15 +2,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int s_sum = 0;
-        int t_sum = 0;
-        for (int i = 0; i < s.length(); i++){
-            s_sum += s[i];
+        int s_sum =0, t_sum = 0;
+        for (char c: s){
+            s_sum += c;
         }
-        for (int j = 0; j < t.length(); j++){
-            t_sum += t[j];
+        for (char c: t){
+            t_sum += c;
         }
-        const char res = t_sum - s_sum;
-        return res;
+       return t_sum-s_sum;
     }
 };
