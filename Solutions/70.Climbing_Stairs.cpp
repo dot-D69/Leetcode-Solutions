32dda42d8bed8d52de53b8891ea1231f6418c814
dot-D69 <anuragvishwas19@gmail.com>
2025-08-07 20:18:21 +0530

    Added solution for  Climbing Stairs

---
+++ Solutions/70.Climbing_Stairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int climb(int i,int n, vector<int> &dp){
+        if(n<=1){
+            return 1;
+        }
+        if(i>n){
+            return 0;
+        }
+        if(i==n){
+            return 1;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+
+        return dp[i] = climb(i+1,n,dp) + climb(i+2,n,dp);
+    }
+
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+
+        int res = climb(0,n,dp);
+        return res;
+    }
+};
