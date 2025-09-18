99455280a2d0051e51497f2eb6097e065476c09d
dot-D69 <anuragvishwas19@gmail.com>
2025-09-18 16:22:27 +0530

    Added brute force solution for 3. finding the length of the longest substring without repeating characters

---
+++ Solutions/3. Longest_Substring_Without_Repeating_Characters(Brute Force).cpp
@@ -0,0 +1,33 @@
+//Brute Force
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int maxLen = 0;
+        for (int i =0;i<n;i++){
+            unordered_set<char> seen;
+            for(int j=i;j<n;j++){
+                if(seen.count(s[j])) break;
+                seen.insert(s[j]);
+                maxLen=max(maxLen,j-i+1);
+            }
+        }
+        return maxLen;
+    }
+};
+
+/*
+
+Explanation:
+Check every possible substring of the string.
+For each substring, verify if all characters are unique.
+Track the maximum length of substrings with unique characters.
+
+Steps:
+Use two loops:
+Outer loop for the starting point of the substring.
+Inner loop for the endpoint of the substring.
+Use a set to check if all characters in the substring are unique.
+Update the maximum length if the substring is valid.
+
+*/
