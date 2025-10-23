034c44a473acf8fabf155522924a422089bfd138
dot-D69 <anuragvishwas19@gmail.com>
2025-10-23 13:32:03 +0530

    Added solution for 3461. Check If Digits Are Equal in String After Operations

---
+++ Solutions/3461. Check_If_Digits_Are_Equal_in_String_After_Operations_I.cpp
@@ -0,0 +1,19 @@
+//Brtue force Simple simulation
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        int n = s.length();
+        while(n>2){
+            for(int i=0;i<n-1;i++){
+                s[i] = ((s[i]-'0'+s[i+1]-'0')%10 )+ '0';
+                
+            }
+            n--;
+        }
+
+        return s[0] == s[1];
+    }
+};
+
+//Time Complexity : O(n^2)
+//Space Complexity: O(1)
