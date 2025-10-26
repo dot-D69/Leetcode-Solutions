44dfd8f695212d87ab7923f537b95c61ee53c9ef
dot-D69 <anuragvishwas19@gmail.com>
2025-10-26 14:08:35 +0530

    Added implementation for Simple Bank System

---
+++ Solutions/2043. Simple_Bank_System.cpp
@@ -0,0 +1,34 @@
+class Bank {
+public:
+    vector<long long> balance;
+    int n;
+    Bank(vector<long long>& balance) {
+        n = balance.size();
+        this->balance = balance;
+    }
+    
+    bool transfer(int account1, int account2, long long money) {
+        if(account1>n || account2 > n || money> balance[account1 -1]){
+            return false;
+        }
+        balance[account1 -1] -= money;
+        balance[account2 -1] += money;
+        return true;
+    }
+    
+    bool deposit(int account, long long money) {
+        if(account > n || account < 1){
+            return false;
+        }
+        balance[account-1]+= money;
+        return true;
+    }
+    
+    bool withdraw(int account, long long money) {
+        if(account > n || account < 1 || money> balance[account -1]){
+            return false;
+        }
+        balance[account-1]-= money;
+        return true;
+    }
+};
