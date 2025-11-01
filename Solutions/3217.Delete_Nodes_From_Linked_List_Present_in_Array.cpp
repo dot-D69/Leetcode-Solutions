6499bcd9c8ee5472a5d4f98d2853dfc82d13c5bb
dot-D69 <anuragvishwas19@gmail.com>
2025-11-01 13:56:01 +0530

    Removed unnecessary delete statements for temporary nodes in the linked list solution

--- Solutions/3217.Delete_Nodes_From_Linked_List_Present_in_Array.cpp
+++ Solutions/3217.Delete_Nodes_From_Linked_List_Present_in_Array.cpp
@@ -19,8 +19,6 @@ public:
         while(head != NULL && st.find(head->val)!=st.end()){ // check if the head value is present int the set and that head is not NULL (if head becomes NULL then we have nothing to check for)
             ListNode* temp =head; // store the head in a temp node
             head= head->next; // update the head to head->next;
-            delete(temp); // delete the temp node having the old head
-
         }
 
         ListNode* curr = head;
@@ -29,7 +27,6 @@ public:
            if(st.find(curr->next->val)!=st.end()){ // if the value of the next of curr is present in the node it means we need to remove it
              ListNode* temp = curr->next; // store the curr->next in a temp node
              curr->next = curr->next->next; // update the curr->next to curr->next->next
-             delete(temp);  // delete the node containing the old curr->next;
            }
            else{ // if the curr->next value is not in the set 
             curr= curr->next; // update the curr to its next
@@ -39,3 +36,9 @@ public:
             return head;    
     }
 };
+
+
+
+// We haven't used delete(temp) in thiss code because lettcode was delteting the the temp itself , and if we deelte it before hand then the code will runtime error,
+
+//But it is generally recomended to delete any dangling nodes, so in interview we will use delete(temp)
